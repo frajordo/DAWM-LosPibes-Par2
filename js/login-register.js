bce6767e5c98e9fb6bcb134b5d1c1fa388550134
Franklin <frajordo@espol.edu.ec>
2021-07-14 21:35:30 -0500

    cosas

---
+++ js/login-register.js
@@ -0,0 +1,15 @@
+
+
+function register(user,email,age,gender,why,password){
+    let url="../jsons/users.json"
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+        let info={"user":user,"email":email,"age":age,"gender":gender,"why":why,"password":password}
+        data["clients"].append(info)
+    })
+}
+
+document.getElementById("button").onclick=function(){
+    register("caca","caca@gmail.com",15,"H","NF","caquita")
+}
