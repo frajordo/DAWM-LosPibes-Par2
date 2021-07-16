38a0de4df7751e62645769f428781e7a6477ba33
ahajcruz <gammaaharon23@gmail.com>
2021-07-15 21:57:21 -0500

    se agrego la funcionalidad de busqueda de productos

--- js/inventaryScript.js
+++ js/inventaryScript.js
@@ -14,7 +14,7 @@ let formato= `<div class="card mb-3 dark-pink text-white">
                   </div>
                </div>`
 
-fetch("../clients.json").
+const extraccionDatos=fetch("../clients.json").
 then(response=> response.json()).
 then(data=>{
     const usuario=localStorage.getItem('user')
@@ -25,8 +25,7 @@ then(data=>{
             document.getElementById("articulos").innerHTML=" "
             document.getElementById("nombreUsuario").innerHTML=data[contador].user
             let inventario=data[contador].inv
-            for( articulo of inventario){
-                console.log(articulo.PImg)
+            for(let articulo of inventario){
                 document.getElementById("articulos").innerHTML +=
             `<div class="card mb-3 dark-pink text-white">
                 <div class="row g-0">
@@ -48,5 +47,40 @@ then(data=>{
             comprobador=false
         }
         contador+=1
-    }
+    }    
+    return data[contador-1].inv 
 }).catch(console.error)
+
+const setSearchEvent= async()=>{
+    const inventario_usuario=await extraccionDatos
+    console.log("posi")
+    document.querySelector('.buscador-inventario').querySelector('input').addEventListener('input', e =>{
+        let criterioBusqueda= document.querySelector('.buscador-inventario').querySelector('input').value
+        let flags
+        flags = 'i'
+        const reg=new RegExp(criterioBusqueda+'.*',flags)
+        document.getElementById("articulos").innerHTML =""
+        for(let articulo of inventario_usuario){
+             if(reg.exec(articulo.PName)){
+                   document.getElementById("articulos").innerHTML +=
+            `<div class="card mb-3 dark-pink text-white">
+                <div class="row g-0">
+                  <div class="col-md-6 col-6">
+                    <img src="${articulo.PImg}"img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-6 col-6 lefty">
+                    <div class="card-body lefty righty">
+                      <h5 class="card-title text-white">${articulo.PName} x${articulo.PI}</h5>
+                      <p class="card-text">categoria: ${articulo.PD}</p>
+                      <p class="card-text">${articulo.PC}</p>
+                      <div class="button">Ver/Editar</div>
+                    </div>
+                  </div>
+                  </div>
+               </div>`
+             }
+            }
+
+    })
+}
+setSearchEvent()
