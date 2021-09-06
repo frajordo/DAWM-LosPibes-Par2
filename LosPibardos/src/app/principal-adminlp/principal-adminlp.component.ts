b95ad755879d375c804af8c11255159d3b8789a8
Marck.MR <maanmuri@espol.edu.ec>
2021-09-06 16:32:34 -0500

    tabla de usuarios panel de adminstrador

--- LosPibardos/src/app/principal-adminlp/principal-adminlp.component.ts
+++ LosPibardos/src/app/principal-adminlp/principal-adminlp.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit,ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-principal-adminlp',
   templateUrl: './principal-adminlp.component.html',
   styleUrls: ['./principal-adminlp.component.css']
 })
+
+
+
 export class PrincipalAdminlpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRef:ElementRef) {}
 
+
+  siftUsers(){
+    let users = this.elementRef.nativeElement.querySelector('#usersByWhyContentTable').children;
+    let selectedOptionWhy =  this.elementRef.nativeElement.querySelector('#usersSelect').selectedOptions[0].value;
+
+    
+    for (let i = 0; i < users.length; i++) {
+      let userWhy = users[i].children[0].innerHTML;
+      if ( userWhy === selectedOptionWhy){
+        users[i].setAttribute("style","");
+      }else{
+        users[i].setAttribute("style","display:none");
+      }
+
+    }
+  }
+
+  ngAfterViewInit() {
+    var table = this.elementRef.nativeElement.querySelector('#usersByWhyContentTable');
+
+    fetch("http://localhost:3000/users")
+    .then(response => response.json())
+    .then(data => {
+
+      for (let i = 0; i < data.length; i++) {
+        table.insertAdjacentHTML('beforeend', `
+        <tr>
+          <td style="display:none" .why >`+ data[i]["why"] +`</td>
+          <th>`+ data[i]["user"] +`</th>
+          <td>`+ data[i]["edad"] +`</td>
+          <td>`+ data[i]["email"] +`</td>
+          <td>`+ data[i]["gender"] +`</td>
+          <td>`+ data[i]["tipo"] +`</td>
+        </tr>`
+        );
+      }
+    })
+    .catch(function(error) {
+      console.log('Hubo un problema con la petición Fetch:' + error.message);
+    });
+
+  }
   ngOnInit(): void {
+
+
   }
 
 }
